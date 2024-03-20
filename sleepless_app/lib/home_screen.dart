9760b3c23253ff8b08e331b6718a52b1ce6e3685
Logan Valeski <loganvaleski@gmail.com>
2024-03-20 13:13:01 +0100

    Background color change. Nightly story button image change. Header logo change.

--- sleepless_app/lib/home_screen.dart
+++ sleepless_app/lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sleepless_app/play_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,26 +10,40 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-// Here, change StatelessWidget to State<HomeScreen>
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sleepless'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Image.asset('assets/images/image.png'),
-          onPressed: () {
-            // Navigate to the PlayScreen
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const PlayScreen()),
-            );
-          },
-        ),
-      ),
-    );
+    return CupertinoPageScaffold(
+        backgroundColor: CupertinoColors.systemIndigo,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start, // Aligns the logo to the top of the screen
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 60.0), // Adjust the padding as needed
+                child: Center(
+                  child: SvgPicture.asset(
+                    'assets/images/logo.svg', // Your logo's asset path
+                    width: 35, // Set your logo's width
+                    height: 35, // Set your logo's height
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: CupertinoButton(
+                    child: SvgPicture.asset('assets/images/playnightly.svg'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const PlayScreen()),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
