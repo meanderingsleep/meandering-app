f2bb8ca75f2c94efb776bff4d7d9256256ec0407
Jud Valeski <jvaleski@gmail.com>
2024-03-24 14:57:27 -0600

    getting some rudimentary tests going.

---
+++ sleepless_app/test/main_app_test.dart
@@ -0,0 +1,14 @@
+import 'package:sleepless_app/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('App has male/female elements', (tester) async {
+    await tester.pumpWidget(const App());
+
+    final mFinder = find.text('Male');
+    final fFinder = find.text('Female');
+
+    expect(mFinder, findsOneWidget);
+    expect(fFinder, findsOneWidget);
+  });
+}
