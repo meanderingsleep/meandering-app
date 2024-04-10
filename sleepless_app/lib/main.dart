05273a2bf707c3e04e555cb31c107721e9a7f57f
Jud Valeski <jvaleski@gmail.com>
2024-04-09 20:09:00 -0600

    forcing portriat mode.

--- sleepless_app/lib/main.dart
+++ sleepless_app/lib/main.dart
@@ -3,12 +3,18 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sleepless_app/home_screen.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       localizationsDelegates: <LocalizationsDelegate<dynamic>>[
