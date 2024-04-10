a7bf56608b53c74772e7538de5a2235e8d1b4843
Jud Valeski <jvaleski@gmail.com>
2024-04-09 19:23:22 -0600

    upgrading just_audio to latest version. removing Provider usage as we weren't using it. cleaning up path_provider as we're not using it. removing associated files.

--- sleepless_app/lib/main.dart
+++ sleepless_app/lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:just_audio/just_audio.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:sleepless_app/app_dependencies/app_dependencies.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sleepless_app/home_screen.dart';
 import 'package:just_audio_background/just_audio_background.dart';
@@ -20,21 +17,13 @@ class App extends StatelessWidget {
         DefaultCupertinoLocalizations.delegate,
       ],
       title: 'Sleepless',
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
 Future main() async {
   await dotenv.load();
-
-  runApp(Provider(
-    create: (_) => AppDependencies(
-      stub: 'test',
-      player: AudioPlayer(),
-    ),
-    child: const App(),
-  ));
 
   await JustAudioBackground.init(
     androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
